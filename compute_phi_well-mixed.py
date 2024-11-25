dc7d67c91d3339e50c3af1b4d3de0dff75e1437c
Noe Prat <noe.prat@student-cs.fr>
2024-11-25 16:30:57 +0100

    implemented a new numerical approach, but issues

---
+++ compute_phi_well-mixed.py
@@ -0,0 +1,99 @@
+import numpy as np
+from scipy.special import comb
+
+### Computing the standard transition matrix
+
+def compute_hypergeometric_prob(N, M, i, k):
+    return comb(i,k)*comb(N-i, M-k) / comb(N,M)
+
+def compute_WF_prob(N,i,j,s):
+    p = i*(1+s) / (i*(1+s) + N-i)
+    if p==0:
+        if j==0:
+            res = 1
+        else:
+            res = 0
+    elif p==1:
+        if j==N:
+            res = 1
+        else:
+            res=0
+    else:
+        res = comb(N,j) * (p**j) * ((1-p)**(N-j))
+    return res
+
+def compute_transition_matrix(N,M,s):
+    P = np.zeros((N+1,N+1), dtype=float)
+    for i in range(N+1):
+        for j in range(N+1):
+            coeff = sum([compute_hypergeometric_prob(N,M,i,k) * compute_WF_prob(M,i,k+j-i,s)
+                         for k in range(i+1)])
+            P[i,j] = coeff
+    return P
+
+
+### Estimating (I-A)^(-1)
+
+def infty_operator_norm(A):
+    u = np.sum(np.abs(A), axis=1)
+    return max(u)
+
+#A = np.array([[0,1],
+#              [1,1]])
+
+#print(infty_operator_norm(A))
+
+def estimate_power(epsilon, A): #should this work ????
+    A_norm = infty_operator_norm(A)
+    assert A_norm != 1
+    res = np.log(epsilon * (1-A_norm)) / np.log(A_norm)
+    return res
+
+def truncated_sum_method(A, power=8):
+    return np.linalg.matrix_power(A, power)
+
+def inverse_method(A):
+    n = A.shape[0]
+    I = np.eye(n)
+    if np.linalg.det(I-A) != 0:
+        return np.linalg.inv(I - A)
+    else:
+        print('I-A not invertible')
+        return truncated_sum_method(A)
+    
+### computing fixation probability
+
+def permutation(i:int, N:int):  # the permutation s.t. P_new[i,j] = P[s(i),s(j)]
+    assert i>=0 and i<=N
+    if i == 0:
+        i_new = N-1
+    elif i == N:
+        i_new = N
+    else:
+        i_new = i-1
+    return i_new  
+
+
+def compute_fixation_probability(N,M,s):
+    P = compute_transition_matrix(N,M,s)
+    P_new = np.zeros_like(P)
+    for i in range(N+1):
+        for j in range(N+1):
+            P_new[i,j] = P[permutation(i,N), permutation(j,N)]
+    
+
+    A = P_new[:N-1, :N-1]
+    print('A:',A)
+    B = P_new[:N-1, N-1:]
+
+    inv = inverse_method(A)  # (I-A)^-1
+    inv_vect = inv[0,:]
+    B_vect = B[:,1]
+
+    return np.inner(inv_vect, B_vect)
+
+
+if __name__ == '__main__':
+
+    print(compute_fixation_probability(10, 10, 0.05))
+    
