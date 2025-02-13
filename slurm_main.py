0583480586245a80469550124dbcf3b9dba69c13
Noe Prat <noe.prat@student-cs.fr>
2025-02-13 16:32:15 +0100

    documentating

--- slurm_main.py
+++ slurm_main.py
@@ -24,7 +24,27 @@ all_types = ['star', 'cycle', 'clique', 'line', 'wm_sim', 'wm_mat']
 
 STORE_FIXATION_TIMES = False
 
-def slurm_main(prefix, results_dir, num, type, job_array_nb, N, M, log_s_min, log_s_max, nb_trajectories=100, migration_rate=0.001, nb_demes=1, alpha=1, initial_node=0):
+def slurm_main(prefix, results_dir, num, type, job_array_nb, N, M, log_s_min, log_s_max, nb_trajectories=100, migration_rate=0.001, nb_demes=1, alpha=1., initial_node=0):
+    """
+    Runs multiple simulations / computations of the specified type, sweeping the fitness in np.logspace(log_s_min, log_s_max, num) and writes a .JSON file in the specified results directory
+    Make sure the results directory is created before running
+    Arguments:
+     -> prefix: str, beginning of the filename, e.g. 'simA'
+     -> results_dir: str, path where the results should be written, e.g. 'results/simA/' (must end with a '/')
+     -> num: int, number of fitness values in the logspace array
+     -> type: str, ['wm_sim', 'wm_mat', 'clique', 'cycle', 'line', 'star']
+     -> job_array_nb: int, identifier (useful for running array jobs with slurm too), will be added in the filename
+     -> N: int, number of individuals per deme
+     -> M: int, number of updated individuals per deme
+     -> log_s_min: int,
+     -> log_s_max: int,
+     -> (optional) nb_trajectories: int = 100, number of simulation runs
+     -> (optional) migration_rate: float = 0.001,
+     -> (optional) nb_demes: int = 1, number of demes,
+     -> (optional) alpha: float = 1., migration assymetry (if type is 'cycle', 'line' or 'star')
+     -> (optional) initial_node: int = 0, node where the first mutant spawns (between 0 and nb_demes - 1), or makes an average if initial_node = 'avg' (for line and star graphs)
+    Returns nothing, writes '{prefix}_{type}_{job_array_nb}_{parameters}.json' in the results directory
+    """
     parameters = {
             'type': type,
             'job_array_nb':job_array_nb,
